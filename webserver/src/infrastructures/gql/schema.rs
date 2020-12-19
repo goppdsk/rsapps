88fbc153d1e998c7067693bcbf15d8644ea52d5b
Koichiro Okamoto <koichirookamoto@icloud.com>
2020-12-19 21:36:48 +0900

    Create graphql server

--- webserver/src/infrastructures/gql/schema.rs
+++ webserver/src/infrastructures/gql/schema.rs
@@ -1,7 +1,34 @@
 use crate::domains::errors::{ApplicationError, ErrorCode};
 use crate::domains::models::user::User;
-use juniper::{FieldError, IntoFieldError, ScalarValue};
+use juniper::http::{graphiql, GraphQLRequest, GraphQLResponse};
+use juniper::{
+    Context, EmptyMutation, EmptySubscription, FieldError, IntoFieldError, RootNode, ScalarValue,
+};
+use lazy_static::lazy_static;
 use std::convert::AsRef;
+use std::sync::{Arc, RwLock};
+use tide::http::mime;
+use tide::{Body, Redirect, Request, Response, Server, StatusCode};
+
+impl<S: ScalarValue> IntoFieldError<S> for ApplicationError {
+    fn into_field_error(self) -> FieldError<S> {
+        let code = self.code.as_ref();
+        match self.code {
+            ErrorCode::SystemError => FieldError::new(
+                self.message,
+                graphql_value!({
+                  "code": code,
+                }),
+            ),
+            _ => FieldError::new(
+                "Un exptected error is occured",
+                graphql_value!({
+                  "code": "UnExptected",
+                }),
+            ),
+        }
+    }
+}
 
 #[graphql_object]
 #[graphql(description = "A user")]
@@ -27,22 +54,57 @@ impl User {
     }
 }
 
-impl<S: ScalarValue> IntoFieldError<S> for ApplicationError {
-    fn into_field_error(self) -> FieldError<S> {
-        let code = self.code.as_ref();
-        match self.code {
-            ErrorCode::SystemError => FieldError::new(
-                self.message,
-                graphql_value!({
-                  "code": code,
-                }),
-            ),
-            _ => FieldError::new(
-                "Un exptected error is occured",
-                graphql_value!({
-                  "code": "UnExptected",
-                }),
-            ),
-        }
+#[derive(Clone)]
+pub struct State {}
+
+impl Context for State {}
+
+pub struct QueryRoot;
+
+#[graphql_object(Context = State)]
+impl QueryRoot {
+    #[graphql(name = "apiVersion")]
+    fn api_version() -> &str {
+        "0.1.0"
     }
+
+    #[graphql(description = "Get all Users")]
+    fn users(context: &State) -> Vec<User> {
+        let users: Vec<User> = Vec::new();
+        users.iter().cloned().collect()
+    }
+}
+
+pub type Schema = RootNode<'static, QueryRoot, EmptyMutation<State>, EmptySubscription<State>>;
+lazy_static! {
+    static ref SCHEMA: Schema =
+        Schema::new(QueryRoot {}, EmptyMutation::new(), EmptySubscription::new());
+}
+
+async fn handle_graphql(mut request: Request<State>) -> tide::Result {
+    let query: GraphQLRequest = request.body_json().await?;
+    let response: GraphQLResponse = query.execute(&SCHEMA, request.state()).await;
+    let status = if response.is_ok() {
+        StatusCode::Ok
+    } else {
+        StatusCode::InternalServerError
+    };
+
+    Ok(Response::builder(status)
+        .body(Body::from_json(&response)?)
+        .build())
+}
+
+async fn handle_graphiql(_: Request<State>) -> tide::Result<impl Into<Response>> {
+    Ok(Response::builder(200)
+        .body(graphiql::graphiql_source("/graphql", None))
+        .content_type(mime::HTML))
+}
+
+pub fn create_graphql_server() -> Server<State> {
+    let mut app = Server::with_state(State {});
+    app.at("/").get(Redirect::permanent("/graphiql"));
+    app.at("/graphql").post(handle_graphql);
+    app.at("/graphiql").get(handle_graphiql);
+    app
 }
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -6,6 +6,11 @@ extern crate strum;
 mod domains;
 mod infrastructures;
 
-fn main() {
-    println!("Hello, world!");
+use infrastructures::gql::schema::create_graphql_server;
+
+#[async_std::main]
+async fn main() -> std::io::Result<()> {
+    let app = create_graphql_server();
+    app.listen("0.0.0.0:8080").await?;
+    Ok(())
 }
