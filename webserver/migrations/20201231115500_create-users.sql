2dad702f44643c96b6ea9957430ec3e5ed319cf5
Koichiro Okamoto <koichirookamoto@icloud.com>
2020-12-31 20:59:52 +0900

    database migration by sql-cli

--- webserver/migrations/20201231115500_create-users.sql
+++ webserver/migrations/20201231115500_create-users.sql
@@ -1,8 +1,9 @@
+-- Add migration script here
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(16) UNIQUE NULL,
-    email VARCHAR(256) UNIQUE NULL,
-    password_hash VARCHAR(256) NULL,
+    username VARCHAR(16) UNIQUE,
+    email VARCHAR(256) UNIQUE,
+    password_hash VARCHAR(256),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
